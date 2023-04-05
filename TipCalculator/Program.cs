218825f83dbeee316176b7fab83ba61ab4f6b79b
Void <Void@DESKTOP-G4T0EEL>
2023-04-05 12:58:53 -0500

    Add project files.

---
+++ TipCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        
+        {
+
+            Console.Write("Enter the total amount of the bill: ");
+            double totalBill = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the percentage of tip you want to leave: ");
+            double tipPercentage = double.Parse(Console.ReadLine());
+
+            double tipAmount = totalBill * (tipPercentage / 100);
+            double totalAmount = totalBill + tipAmount;
+
+            Console.WriteLine($"Tip amount: {tipAmount:C}");
+            Console.WriteLine($"Total amount (including tip): {totalAmount:C}");
+
+            Console.ReadLine();
+        }
+    }
+}
